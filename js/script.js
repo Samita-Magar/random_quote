09180d2fc13f3a8f4ed1a741366858eca5078caa
Samita-Magar <133216948+Samita-Magar@users.noreply.github.com>
2023-05-12 20:48:38 +1000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [ 
+  {
+    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    source: 'Nelson Mandela',
+    citation: 'Long Walk To Freedom',
+    year: 1995
+  }, 
+  {
+    quote: 'Spread love everywhere you go. Let no one ever come to you without leaving happier',
+    source: 'Mother Teresa' 
+  }, 
+  {
+    quote: 'The future belongs to those who believe in the beauty of their dreams.',
+    source: 'Eleanor Roosevelt'
+  }, 
+  {
+    quote: 'Never let the fear of striking out keep you from playing the game',
+    source: 'Babe Ruth'
+  }, 
+  {
+    quote: 'Life is either a daring adventure or nothing at all',
+    source: 'Helen Keller'
+  }, 
+  {
+    quote: 'May the force be with you',
+    source: 'Star Wars',
+    citation: 'Star Wars Movie',
+    year: 1977
+  },
+  {
+    quote: 'Work for works sake, not for yourself. Act but do not be attached to your actions. Be in the world, but not of it',
+    source: 'Bhagvad Gita'
+  }
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+function getRandomQuote() {
+
+
+}
+
+
+/***
+ * `printQuote` function
+***/
+function printQuote() {
+
+
+}
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
